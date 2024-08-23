842d5600154c66c975fd5bfc01f689f9039f30d5
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-08-23 13:47:56 +0200

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Space_Mono } from 'next/font/google';
+import './globals.css';
+
+const spacemono = Space_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : 'http://localhost:3000';
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: 'OZELOT CLORGA',
+  description: 'The fastest way to build apps with Next.js and Supabase',
 };
 
 export default function RootLayout({
@@ -17,11 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+    <html lang="en">
+      <body className={spacemono.className}>
+        <main>{children}</main>
       </body>
     </html>
   );
