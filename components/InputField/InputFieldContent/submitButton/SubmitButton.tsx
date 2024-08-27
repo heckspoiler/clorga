462bdae228edc77bc80e361abcef15a20e4e7319
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-08-27 16:31:37 +0200

    changes

--- components/InputField/InputFieldContent/submitButton/SubmitButton.tsx
+++ components/InputField/InputFieldContent/submitButton/SubmitButton.tsx
@@ -21,21 +21,18 @@ export default function SubmitButton({ styles }: { styles: any }) {
         due_date: projectDueDate,
         project_ideas: [
           {
-            id: 3,
             tags: ['healthcare', 'AI', 'research'],
             title: 'Idea 3',
             created_at: '2023-08-27T09:30:00Z',
             description: 'Description of idea 3',
           },
           {
-            id: 4,
             tags: ['education', 'online-learning', 'EdTech'],
             title: 'Idea 4',
             created_at: '2023-08-27T14:45:00Z',
             description: 'Description of idea 4',
           },
           {
-            id: 5,
             tags: ['finance', 'blockchain', 'cryptocurrency'],
             title: 'Idea 5',
             created_at: '2023-08-28T11:15:00Z',
