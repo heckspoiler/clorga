0e37245acafe2023f39a50763a1a317e516afc88
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-10-03 14:56:21 +0200

    changes

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,9 +1,9 @@
-// 'use client';
-// import React from 'react';
-// import { userStore } from '@/utils/userStore';
+'use client';
+import React from 'react';
+import { userStore } from '@/utils/userStore';
 
-// export default function ErrorPage() {
-//   const firstName = userStore((state) => state.firstName); // Use useStore() to trigger reactivity
+export default function ErrorPage() {
+  const firstName = userStore((state) => state.firstName); // Use useStore() to trigger reactivity
 
-//   return <p>Sorry {firstName}, something went wrong</p>;
-// }
+  return <p>Sorry {firstName}, something went wrong</p>;
+}
